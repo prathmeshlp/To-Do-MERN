18bb65359e5ffce5df07fdfed4472b5fa0b267bc
Prathmesh Mulhar <prathmeshmulhar@gmail.com>
2023-10-03 14:56:20 +0530

    Added Changes

--- Backend/server.js
+++ Backend/server.js
@@ -5,22 +5,26 @@ const config = require('./config')
 const ToDoRoutes = require("./routes/ToDoRoutes")
 const UserRoutes = require('./routes/UserRoutes')
 
-
-
 // connect to express app
 const app = express();
 
 // middleware
 app.use(express.json());
 const corsOptions = {
-  origin: 'https://to-do-app-with-auth.vercel.app',
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  credentials: true, // If you need to include cookies in your requests
+  origin: ['https://todoapp.vercel.app'],
+  methods: ["GET","HEAD","PUT","PATCH","POST","DELETE"],
+  // credentials: true, // If you need to include cookies in your requests
 };
 app.use(cors(corsOptions))
-
 app.use("/", UserRoutes);
 app.use("/api", ToDoRoutes);
+
+
+app.get('*',(req,res,next)=>{
+  res.status(200).json({
+    message:'bad request'
+  })
+})
 
 
 // connect to mongoDB
